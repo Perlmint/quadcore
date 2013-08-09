f1324af45500a7f3336fba0156d5862bc228258f
asdefary dea <asdefary@gmail.com>
2013-08-10 03:54:32 +0900

    Scripting

---
+++ runner.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import script
+
+class Runner:
+	def narr(self, s):
+		pass
+
+	def conv(self, s):
+		pass
+
+	def choice(self, s):
+		return 0
+
+	def end(self):
+		pass
+
+	def callback(self, s):
+		s.callback(s.data)
+		pass
+
+	def show(self, s):
+		pass
+
+	def hide(self, s):
+		pass
+
+	def back(self, s):
+		pass
+
+class CliRunner(Runner):
+	def __init__(self, name):
+		self.name = name
+
+	def narr(self, s):
+		print s
+		raw_input()
+
+	def conv(self, s):
+		if isinstance(s.name, script.Self):
+			name = self.name
+		else:
+			name = s.name
+
+		print name + ":\t" + s.text
+		raw_input()
+
+	def choice(self, s):
+		print "선택지: " + s.question
+		print
+
+		no = 0
+
+		for i in s.selections:
+			print "%d %s" % (no, i.name)
+			no += 1
+
+		return input("선택: ")
+
+t = script.ScriptInterpreter(script.test)
+runner = CliRunner("히로인")
+
+while t.run(runner):
+	pass
+
+
--- script.py
+++ script.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
+class Self:
+	pass
+
+class Conversation:
+	def __init__(self, name, text):
+		self.name = name
+		self.text = text
+
 class Selection:
 	def __init__(self, name, script):
 		self.name   = name
@@ -14,20 +22,73 @@ class EndScript:
 	pass
 
 class CallbackScript:
-	def __init__(self, callback):
+	def __init__(self, callback, data):
 		self.callback = callback
+		self.data = data
+
+class ShowHeroine:
+	def __init__(self, image):
+		self.image = image
+
+class HideHeroine:
+	def __init__(self, image):
+		self.image = image
+
+class Background:
+	def __init__(self, image):
+		self.image = image
+
+
+class ScriptInterpreter:
+	def __init__(self, script):
+		self.script = script
+	
+	def run(self, runner):
+		if not self.script:
+			return False
+
+		s = self.script.pop(0)
+
+		if isinstance(s, str):
+			runner.narr(s)
+		elif isinstance(s, Conversation):
+			runner.conv(s)
+		elif isinstance(s, Choice):
+			res = runner.choice(s)
+
+			self.script = s.selections[res].script
+		elif isinstance(s, EndScript):
+			runner.end()
+			self.script = list()
+		elif isinstance(s, CallbackScript):
+			runner.callback(s)
+		elif isinstance(s, ShowHeroine):
+			runner.show(s)
+		elif isinstance(s, HideHeroine):
+			runner.hide(s)
+		elif isinstance(s, Background):
+			runner.back(s)
+
+		return True
+
 
 die_script = [
 	"푹찍",
 	EndScript(),
 ]
 
+live_script = [
+	"도망",
+	EndScript(),
+]
+
 test = [
-	"나는 너가 좋아",
-	"그러니 죽어주면 좋겠어",
+	Conversation(Self(), "나는 너가 좋아"),
+	Conversation(Self(), "그러니 죽어주면 좋겠어"),
+	"죽어야 하나 고민이네",
 	Choice("죽을까", [
 		Selection("죽는다", die_script),
-		Selection("도망간다", die_script),
+		Selection("도망간다", live_script),
 	])
 ]
 
