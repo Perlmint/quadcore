af16a6c99bb3176dcbac437b4f94736a198ae8fe
Gyu-sun Youm <omniavinco@gmail.com>
2013-08-11 07:40:08 +0900

    over layer추가, shop 추가

---
+++ src/maps/shop.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+player = {'pos':(32 + 16, 32 * 2)}
+map = {'filename':'inner_shop.tmx'}
+event = [
+{'pos':(32 * 2, 32 * 3), 'action':'town'},
+]
+heroine = [
+{'name':"sekai", 'pos':(32 * 1, 32 * 2), 'probability':0.8}
+]
