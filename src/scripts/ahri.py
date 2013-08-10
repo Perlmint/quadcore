9e5fa4a2145fffacc13a0176491b51f4d8604742
Jang Seung Ho <shjang1992@hotmail.com>
2013-08-11 08:24:16 +0900

    Ahri Update

--- src/scripts/ahri.py
+++ src/scripts/ahri.py
@@ -43,15 +43,17 @@ script = [
 	Route("Dead End", DieRightAway,
 	[
 		[Conversation(Self(), u"너는 죽었어♥"),
-			Choice(u"죽을까", [
+			Choice(u"죽을까",
+			[
 				Selection(u"죽는다", die_script),
 				Selection(u"도망간다", live_script)
-				]),
-
+			]),
 			EndScript()
 		],
 		[
 			Conversation(Self(), u"죽어!!"),
+			u"당신은 죽었습니다.",
+			u"게임오버",
 			CallbackScript(GameOver, None)
 		]
 	])
