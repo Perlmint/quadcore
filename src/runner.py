e11b06c4b562ed91dee77f64ff67f39446549502
Jang Seung Ho <shjang1992@hotmail.com>
2013-08-10 18:17:11 +0900

    GameRunner added

--- src/runner.py
+++ src/runner.py
@@ -102,4 +102,57 @@ class CliRunner(Runner):
 	def love(self, s):
 		self.heroine.love += s.val
 
+class GameRunner(Runner):
+	def __init__(self, loveee, heroine, place, dialog):
+		self.loveee  = loveee
+		self.heroine = heroine
+		self.place   = place
+		self.dialog = dialog
 
+	def narr(self, s):
+		print s
+		dialog.setMessage({"msgList" : [s]})
+
+	def conv(self, s):
+		if isinstance(s.name, script.Self):
+			name = self.heroine.name
+		else:
+			name = s.name
+
+		self.dialog.setMessage({"msgList" : [name + ":\t" + s.text], "image" : "cat.gif"})
+
+	def choice(self, s):
+		print "선택지: " + s.question
+		dialog.setMessage({"msgList" : ["선택지: " + s.question]})
+
+		no = 0
+
+		for i in s.selections:
+			print "%d %s" % (no, i.name)
+			no += 1
+
+		return input("선택: ")
+
+	def take(self, s):
+		print s.item.name + "을 받았다!"
+
+	def give(self, s, script):
+		if not self.loveee.player.items:
+			print "줄 아이템이 없다"
+			return script
+
+		print "무엇을 줄까:"
+		print
+
+		no = 0
+
+		for i in self.loveee.player.items:
+			print "%d % " % (no, i.name)
+			no += 1
+
+		res = input("선택: ")
+
+		return script
+
+	def love(self, s):
+		self.heroine.love += s.val
