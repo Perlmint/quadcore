a1b063ea024784a3bc9b59e8bd9412f4fdce95f5
Gyu-sun Youm <omniavinco@gmail.com>
2013-08-11 13:24:55 +0900

    젠 위치 보정 + 입구 보정 + 에라이 모르겠

--- src/maps/shop.py
+++ src/maps/shop.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-player = {'pos':(32 + 16, 32 * 2)}
+player = {'pos':(32 * 2.5, 32 * 2.5)}
 map = {'filename':'inner_shop.tmx'}
 event = [
 {'pos':(32 * 2, 32 * 3), 'action':'town'},
 ]
 heroine = [
-{'name':"sekai", 'pos':(32 * 1, 32 * 2), 'probability':0.8}
+{'name':"sekai", 'pos':(32 * 4, 32 * 1.5), 'probability':0.8}
 ]
